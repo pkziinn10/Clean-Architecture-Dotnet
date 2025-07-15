e4d71a8f9efda52efe25677798dbf3aa2657fda4
pkziinn10 <pedrinhokauan824@gmail.com>
2025-07-15 08:41:28 -0300

    feat: implementing UseCases

--- CleanArchitecture.Domain/obj/Debug/net8.0/CleanArchitecture.Domain.AssemblyInfo.cs
+++ CleanArchitecture.Domain/obj/Debug/net8.0/CleanArchitecture.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchitecture.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7eb37af626ebccd0f944f69b9decb6c29afadb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d16bbfc6bc462410501ec69f9295544dfd46acf6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanArchitecture.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanArchitecture.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
